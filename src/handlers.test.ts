b83260a2830ea75c9c1672c611daad04c1573b6a
brennop <brennopcordeiro@gmail.com>
2020-05-18 16:49:11 -0300

    ⭐ Adiciona validação para tipos de mensagens
    Apenas mensagens normais devem engatilhar o bot. Eventos de entrar no
    canal ou editar mensagem devem ser ignorados pelo bot.
    
    Respostas a threads devem ser ignoradas também, e esse código deveria
    cobrir este caso, mas há possívelmente um bug na biblioeteca [1]
    
    [1] https://github.com/slackapi/node-slack-sdk/issues/1019

--- src/handlers.test.ts
+++ src/handlers.test.ts
@@ -1,15 +1,30 @@
 import mockAxios from "axios";
 import { onMessage } from "./handlers";
 
+beforeEach(() => {
+  jest.clearAllMocks();
+});
+
 it("calls axios with basic information", () => {
   const event = {
     user: "U0138KPPPP1",
     text: "<@U013GNX05AA> mandou bem me ajudando",
   };
   onMessage(event);
+  expect(mockAxios.post).toBeCalledTimes(1);
   expect(mockAxios.post).toBeCalledWith("/did_goods", {
     sender_id: 1,
     receiver_id: 2,
     description: "mandou bem me ajudando",
   });
+});
+
+it("stops when message is a reply/something else", () => {
+  const event = {
+    subtype: "message_replied",
+    user: "U0138KPPPP1",
+    text: "<@U013GNX05AA> mandou bem me ajudando",
+  };
+  onMessage(event);
+  expect(mockAxios.post).not.toBeCalled();
 });
--- src/handlers.ts
+++ src/handlers.ts
@@ -2,6 +2,11 @@ import { newDidGood } from "./services/api";
 import { getUser, splitMessage } from "./utils";
 
 export const onMessage = (event) => {
+  // should stop if there's a subtype eg. thread reply, channel join
+  if (event.subtype) {
+    return;
+  }
+
   const { key: sender_id } = getUser(event.user);
 
   if (event.text) {
